c5f9b9a28c50e629b4bf0b20a28161e12fea8351
steelbsteel <mrtrecer@bk.ru>
2024-02-26 17:59:58 +0300

    Added class to output product info

---
+++ Lopushok/Lopushok/Class/ProductOutputClass.cs
@@ -0,0 +1,30 @@
+﻿using Lopushok.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Lopushok.Class
+{
+    public class ProductOutputClass
+    {
+        public Product product { get; set; }
+        public string materials { get; set; }
+
+        public int cost { get; set; }
+
+        public ProductOutputClass(Product _product)
+        {
+            product = _product;
+            List<Product_Material> matrsl = App.connection.Product_Material.Where(x => x.idProduct == _product.idProduct).ToList();
+            foreach(var matr in matrsl)
+            {
+                materials += $"{App.connection.Material.FirstOrDefault(x => x.idMaterial == matr.idMaterial).Title}, ";
+                cost += App.connection.Material.FirstOrDefault(x => x.idMaterial == matr.idMaterial).Cost;
+        }
+            materials.Remove(materials.Length - 2);  
+        }
+    }
+}
